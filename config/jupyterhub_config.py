bf71cdd25d6adcdc2b2145215f3ec6d526753138
Justin Holmes <jholmes@redhat.com>
2018-01-26 13:23:53 -0700

    remove whitelist

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,6 +1,6 @@
 import os
 
 c.Authenticator.admin_users = {'jholmes'}
-c.Authenticator.whitelist = {'jholmes','obedin','jholmes-tester','djurgens'}
+# c.Authenticator.whitelist = {'jholmes','obedin','jholmes-tester','djurgens'}
 c.LocalAuthenticator.create_system_users = True
 c.JupyterHub.authenticator_class = 'oauthenticator.openshift.OpenShiftOAuthenticator'
