631c533cf23db103b7d1048a64d2e813526e6f13
Justin Holmes <jholmes@redhat.com>
2018-01-26 12:57:41 -0700

    move to oauth

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -3,4 +3,4 @@ import os
 c.Authenticator.admin_users = {'jholmes'}
 c.Authenticator.whitelist = {'jholmes','foo'}
 c.LocalAuthenticator.create_system_users = True
-c.JupyterHub.authenticator_class = 'jupyterhub.auth.LocalAuthenticator'
+c.JupyterHub.authenticator_class = 'oauthenticator.openshift.OpenShiftOAuthenticator'
