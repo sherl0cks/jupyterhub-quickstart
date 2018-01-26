64f11b3cb45fb39c6754a8080dd5ce2bcb6d43a2
Justin Holmes <jholmes@redhat.com>
2018-01-26 12:28:02 -0700

    update2

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,3 +1,6 @@
 import os
 
 c.Authenticator.admin_users = {'jholmes'}
+c.Authenticator.whitelist = {'jholmes','foo'}
+c.LocalAuthenticator.create_system_users = True
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.LocalAuthenticator'
